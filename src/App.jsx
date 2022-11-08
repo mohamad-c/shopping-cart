eb7b37efc524c3ca787dc4524990edb114d534c3
Mohammad <myarali1345@gmail.com>
2022-11-08 21:47:57 +0330

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <ProductPage />
     </div>
   );
